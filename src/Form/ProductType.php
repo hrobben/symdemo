cd2f58cbd1d06178e076ce55dff1a7f464d4d8a7
sys72740hro <Hcbdag@635>
2022-12-22 10:15:42 +0100

    Vich uploader front-end toevoeging.

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -6,6 +6,7 @@ use App\Entity\Product;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichFileType;
 
 class ProductType extends AbstractType
 {
@@ -15,6 +16,13 @@ class ProductType extends AbstractType
             ->add('code')
             ->add('description')
             ->add('price')
+            ->add('imageFile', VichFileType::class,  [
+                'required' => false,
+                'label_format' => "File",
+                'label' => "File",
+                'allow_delete' => false,
+                'asset_helper' => true,
+            ])
         ;
     }
 
